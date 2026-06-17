936058c935b343ec0e8db2faa7de5d4af128b3cb
Nikita Denisov <nadenisov@gmail.com>
2026-06-17 14:26:34 +0300

    fix: correct PluginServiceRegistrator namespace and signature
    - Use MediaBrowser.Controller.Plugins (not Common.Plugins)
    - Add IServerApplicationHost parameter to RegisterServices
    - Recreate file that was deleted during build error investigation

--- PluginServiceRegistrator.cs
+++ PluginServiceRegistrator.cs
@@ -1,13 +1,13 @@
 using Jellyfin.Plugin.UserRatings.Data;
 using Jellyfin.Plugin.UserRatings.Services;
-using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Jellyfin.Plugin.UserRatings
 {
     public class PluginServiceRegistrator : IPluginServiceRegistrator
     {
-        public void RegisterServices(IServiceCollection serviceCollection)
+        public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
         {
             serviceCollection.AddSingleton<RatingRepository>();
             serviceCollection.AddSingleton<ProgressTracker>();
