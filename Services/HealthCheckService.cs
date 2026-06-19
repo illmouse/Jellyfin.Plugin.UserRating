93e2df427a15db0d764cd57dfea64a45b1bce6aa
Nikita Denisov <nadenisov@gmail.com>
2026-06-19 15:48:08 +0300

    fix: add missing BaseItem using directive, use numeric version for MSBuild (1.9.0.2)

--- Services/HealthCheckService.cs
+++ Services/HealthCheckService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Jellyfin.Plugin.UserRatings.Data;
 using Jellyfin.Plugin.UserRatings.Models;
+using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Library;
 using Microsoft.Extensions.Logging;
 
